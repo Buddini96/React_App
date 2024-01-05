d4dc621505b047c6f81255e55e9191984375e5a5
buddini96 <buddinikl1996@gmail.com>
2024-01-05 12:33:06 +0530

    content updated

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,35 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const productsList = require('../constants/const')
 
 /* GET users listing. */
 router.get('/all', function(req, res, next) {
-  res.send(
-      [
-        {
-          "id" : 1,
-          "name" : "Spinach",
-          "price" : "200",
-          "currency" : "LKR",
-          "image" : "spinach.webp"
-        },
-
-        {
-          "id" : 2,
-          "name" : "Tomatoes",
-          "price" : "540",
-          "currency" : "LKR",
-          "image" : "tomato.webp"
-        },
-
-        {
-          "id" : 3,
-          "name" : "Beans",
-          "price" : "375",
-          "currency" : "LKR",
-          "image" : "beans.jpg"
-        }
-      ]
-  );
+  res.send(productsList);
 });
 
 module.exports = router;
